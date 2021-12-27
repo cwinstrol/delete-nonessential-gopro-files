dd7c1155957b208c6c939ce288ca102926039b68
J. Chris Wingeier II <52218669+jcwii@users.noreply.github.com>
2021-12-27 15:48:47 -0500

    Update delete-nonessential-gopro-files.py

--- delete-nonessential-gopro-files.py
+++ delete-nonessential-gopro-files.py
@@ -1,1 +1,7 @@
-
+import os
+#r=''
+def of(r):
+    for p,ds,fs in os.walk(r):
+        for f in fs:
+            if f.endswith(['.gpr', '.lrv', '.thm']):
+                os.remove(os.path.normpath(p+'\\'+f))
